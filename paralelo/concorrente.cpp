71535496a81334bb476a9ebebb4d8b28ee346e19
Maycon Carvalho <maycon.arth.carvalho@gmail.com>
2021-03-23 09:01:39 -0300

    comentando o codigo concorrente

--- paralelo/concorrente.cpp
+++ paralelo/concorrente.cpp
@@ -4,16 +4,19 @@
 
 using namespace std;
 
-// Define o tamanho da matriz quadrada a ser preenchida
+// Define o tamanho da vetor de primos
 #define TAM 15
+// Define a quantidade de threads a serem usadas
 #define NTHREADS 6
-
+// Define a variável para o controle da região crítica
 pthread_mutex_t mutex;
-
-// Cria a referência global para a vetor com os numeros primos
+// Cria vetor para alocar os números primos
 int *vetor_primos;
+// Define variável de índice para inserir no vetor
 int indice = 0;
 
+// função que determina se o número é primo e adiciona no
+// vetor de primos
 void *calcula_primo(void *aux) {
     int *num = (int *) aux;
     int is_primo = 1;
@@ -33,6 +36,7 @@ void *calcula_primo(void *aux) {
     return 0;
 }
 
+// função que imprimi o vetor de primos
 void print_vetor() {
     printf("print vetor de primos primos\n");
     for (int i = 0; i < indice; i++) {
@@ -41,14 +45,19 @@ void print_vetor() {
     printf("\n");
 }
 
+// método main
 int main(int argc, char** argv) {
+    // aloca espaço do vetor de primos na memória
     vetor_primos = (int*) malloc(sizeof (int*) * TAM);
 
+    // inicia a variável de controle de região crítica
     pthread_mutex_init(&mutex, NULL);
 
+    // cria um vetor de threads de tamanho NTHREADS
     pthread_t threads[NTHREADS];
     int *num;
 
+    // laço para disparar todas as threads
     for (int i = 0; i < NTHREADS; i++) {
         num = (int *) malloc(sizeof (int));
         *num = i + 1;
@@ -59,6 +68,7 @@ int main(int argc, char** argv) {
         }
     }
 
+    // laço para esperar o término de todas as threads
     for (int i = 0; i < NTHREADS; i++) {
         if (pthread_join(threads[i], NULL)) {
             printf("--ERRO: pthread_join() \n");
