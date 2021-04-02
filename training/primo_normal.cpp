cfff7776a9fdd5fc0714dcdded2b1d2ce31ebb8e
adrlvma <adrlvma741@gmail.com>
2021-04-01 22:22:49 -0300

    Definindo teste de primos.

--- training/primo_normal.cpp
+++ training/primo_normal.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 
+
+int teste_primo(int numero){
+    int counter = 0;
+
+    for (int i = 1; i < numero/2; i++)
+    {
+        
+        if (numero % i > 0)
+        {
+            counter++;
+        }
+
+    }
+    
+    if (counter>0)
+    {
+        return 0;
+    }
+    
+
+    return 1;
+}
+
 int main(int argc, char const *argv[])
 {
 
     for (int i = 0; i < 60; i++)
     {
-        printf("Aqui!!!\n");
+        
+        printf("Número: %d ---", i);
+        printf("Resultado do teste: %d.", teste_primo(i));
+
     }
     
 
