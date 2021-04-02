2dffc3d20a97231fc42f671700527864e216ba05
adrlvma <adrlvma741@gmail.com>
2021-04-02 11:51:59 -0300

    Definindo função para imprimir o vetor de primos.

--- training/primo_paralelo.cpp
+++ training/primo_paralelo.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
+void imprime_primos(int* vetor_primos, int numero_maximo)
+{
+    
+    for (int i = 0; i < numero_maximo; i++)
+    {
+        printf("%d     ", vetor_primos[i]);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 
-    int numero_maximo = 500;
-
     //Vetor de Primos
+    int numero_maximo = 500;
     int* vetor_primos = (int*) malloc(sizeof(int*) * numero_maximo);
+
+    for (int i = 0; i < numero_maximo; i++)
+    {
+        vetor_primos[i] = i+1;
+    }
+
+    imprime_primos(vetor_primos, numero_maximo);
 
 
     return 0;
