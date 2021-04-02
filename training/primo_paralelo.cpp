574b03ba8f31e397bc920ff5cb749383d4f12bd4
adrlvma <adrlvma741@gmail.com>
2021-04-02 11:27:44 -0300

    Criando vetor em C++.

---
+++ training/primo_paralelo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+
+    int numero_maximo = 500;
+
+    //Vetor de Primos
+    int* vetor_primos = (int*) malloc(sizeof(int*) * numero_maximo);
+
+
+    return 0;
+}
