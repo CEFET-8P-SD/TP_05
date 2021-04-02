5f658126b7bc3ad150d8f5f73a31c7c342a1a809
adrlvma <adrlvma741@gmail.com>
2021-04-02 00:36:55 -0300

    Corrigindo cálculo de primos.

--- training/primo_normal.cpp
+++ training/primo_normal.cpp
@@ -4,21 +4,15 @@
 int teste_primo(int numero){
     int counter = 0;
 
-    for (int i = 1; i < numero/2; i++)
+    for (int i = 2; i < numero/2+1; i++)
     {
         
-        if (numero % i > 0)
+        if (numero % i == 0)
         {
-            counter++;
+            return 0;
         }
 
     }
-    
-    if (counter>0)
-    {
-        return 0;
-    }
-    
 
     return 1;
 }
@@ -26,11 +20,11 @@ int teste_primo(int numero){
 int main(int argc, char const *argv[])
 {
 
-    for (int i = 0; i < 60; i++)
+    for (int i = 3; i <= 60; i++)
     {
-        
+
         printf("Número: %d ---", i);
-        printf("Resultado do teste: %d.", teste_primo(i));
+        printf("Resultado do teste: %d.\n", teste_primo(i));
 
     }
     
